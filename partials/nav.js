8b38c75fc52329a59c7797489bef1b7a493dd18f
andygimma <andy.gimma@kiron.ngo>
2017-05-06 10:04:27 -0400

    Fix ul/li CSS to only affect donate button. Add nav partial support.

---
+++ partials/nav.js
@@ -0,0 +1,43 @@
+$(document).ready(function()
+{
+  $(".nav").append(nav);
+});
+
+var nav = '<a href="/"><h1><span>#</span>Knock<span>Every</span>Door</h1></a> \
+<ul> \
+  <li> \
+    <a href="/about/index.html">About</a> \
+    <ul class="fallback"> \
+      <li><a href="#">What is Knock Every Door?</a></li> \
+      <li><a href="#">Our Team</a></li> \
+      <li><a href="#">FAQ</a></li> \
+      <li><a href="#">Contact Us</a></li> \
+    </ul> \
+  </li> \
+  <li> \
+    <a href="/how-it-works/index.html">How it works</a> \
+    <ul class="fallback"> \
+      <li><a href="#">How it works</a></li> \
+      <li><a href="#">Deep canvassing 101</a></li> \
+    </ul> \
+  </li> \
+  <li> \
+    <a href="/get-started/index.html">Get started</a> \
+    <ul class="fallback"> \
+      <li><a href="#">Upcoming Calls</a></li> \
+      <li><a href="#">Resources</a></li> \
+    </ul> \
+  </li> \
+  <li><a href="https://actionnetwork.org/event_campaigns/knockeverydoor" target="_blank">Find an Event</a></li> \
+  <li> \
+    <a href="/press/index.html">Press</a> \
+    <ul class="fallback"> \
+      <li><a href="#">News Coverage</a></li> \
+      <li><a href="#">Press Inquiries</a></li> \
+    </ul> \
+  </li> \
+  <li><a href="https://secure.actblue.com/contribute/page/kedthanks?refcode=homepage" class="donate">DONATE</a></li> \
+  <div class="clear"></div> \
+</ul> \
+<hr> \
+<div class="clear"></div>';
